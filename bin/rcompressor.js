1e894be9bd92e3e6300f295b285ec130b6bf6ff7
Zeljko Simic <zeljkosbgd@gmail.com>
2015-02-13 17:45:06 +0100

    Created rCompressor

--- bin/rcompressor.js
+++ bin/rcompressor.js
@@ -1,3 +1,20 @@
 #!/usr/bin/env node
 
-var packageData = require(__dirname + "/../package.json");
+var RCompressor = require("../lib/rcompressor"),
+	commander     = require("commander"),
+	packageData   = require(__dirname + "/../package.json");
+
+	commander.version(packageData.version)
+		.usage("[options] [dir]")
+		.option('-a, --action <n>',     'action - compress/revert')
+		.option('-c, --config <n>',     'config file path')
+		.option('-u, --baseUrl <n>',     'base url')
+		.parse(process.argv);
+
+config = require(commander.config);
+config.baseUrl = commander.baseUrl;
+
+var rCompressor = new RCompressor();
+rCompressor.request.config	= config;
+rCompressor.request.action	= commander.action;
+rCompressor.run();
